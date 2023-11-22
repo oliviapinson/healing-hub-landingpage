eb2908c7286447ce89abee856f864266bc72fc73
Olivia Pinson <olivia.pinson@ufl.edu>
2023-11-22 18:02:57 -0500

    vid

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 
 export const Header = (props) => {
+  // const myStyle = {
+  //   paddingLeft: "50px"
+  // };
+
   return (
     <header id="header">
       <div className="intro">
+      
         <div className="overlay">
+        
           <div className="container">
+          
             <div className="row">
+              
               <div className="col-md-8 col-md-offset-2 intro-text">
+              <div className="col-md-4">
+                <div className="video-container">
+                  <iframe
+                    width="412"
+                    height="732"
+                    src="https://www.youtube.com/embed/qTZA4vRrvXw?autoplay=1&loop=1"
+                    title="updateFigv1"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    // allowFullScreen
+                  ></iframe>
+                </div>
+              </div>
                 <h1>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
@@ -20,6 +41,7 @@ export const Header = (props) => {
                   Learn More
                 </a>{" "}
               </div>
+              
             </div>
           </div>
         </div>
@@ -27,3 +49,4 @@ export const Header = (props) => {
     </header>
   );
 };
+
